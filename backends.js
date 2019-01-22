4371a2e52c10c3161c29d6ac33dbb9fcb9376000
kl9450 <plamediluyeye@gmail.com>
2019-01-22 11:31:25 +0200

    commit

---
+++ backends.js
@@ -0,0 +1,14 @@
+// Request data from a backend
+
+// Put simply, HTTP is a protoco which allows us to send information around the web. For example, when your browser asks for http://domain. HTTP requests come in different flavors, using different verbs
+
+
+
+
+// AJAX stands for Asynchronous JavaScript And XML. Now there's a mouthful.
+
+// Basically, it is a stystem that allows us to send requests an handle the respenses from whithin our Javascript code. For example, when you leave a comment on a blog post, when you send or receive
+
+// Two other initialisms you may see in this context are API and JSON.
+
+// API stands for Application Programming Interface. Many companies and services provide APIs which will react in specific ways when we send them HTTP requests. You 
